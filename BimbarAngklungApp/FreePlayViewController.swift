1c65c901197f200ddd3024fa2728a4356a1e9eee
Fabian Delano Kurnia <fabiandkurnia@gmail.com>
2021-06-15 00:45:29 +0700

    Added AV Foundation to Free Play Screen

--- BimbarAngklungApp/FreePlayViewController.swift
+++ BimbarAngklungApp/FreePlayViewController.swift
@@ -6,15 +6,107 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FreePlayViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        freePlayImage.image = UIImage(named: "AngklungTest")
+        angklungNote = 0
         // Do any additional setup after loading the view.
     }
     
-
-
+    @IBOutlet weak var freePlayImage: UIImageView!
+    
+    var angklungNote: Int = 0
+        
+    var angklungSound = AVAudioPlayer()
+    
+    @IBAction func noteC4(_ sender: Any) {
+        angklungNote = 1
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "C4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteD4(_ sender: Any) {
+        angklungNote = 2
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "D4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteE4(_ sender: Any) {
+        angklungNote = 3
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "E4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteF4(_ sender: Any) {
+        angklungNote = 4
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "F4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteG4(_ sender: Any) {
+        angklungNote = 5
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "G4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteA4(_ sender: Any) {
+        angklungNote = 6
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "A4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteB4(_ sender: Any) {
+        angklungNote = 7
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "B4", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func noteC5(_ sender: Any) {
+        angklungNote = 8
+            do{
+                angklungSound = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "C5", ofType: "aiff") ?? ""))
+            } catch {
+                print("Error")
+            }
+        angklungSound.prepareToPlay()
+    }
+    
+    @IBAction func testPlayButton(_ sender: Any) {
+        angklungSound.play()
+    }
+    
+    
+    
 }
