2f0e61f14d178288312c1b9693033d30beffc1ac
Fabian Delano Kurnia <fabiandkurnia@gmail.com>
2021-06-15 00:33:33 +0700

    Image user interaction set to true

---
+++ BimbarAngklungApp/FreePlayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FreePlayViewController.swift
+//  BimbarAngklungApp
+//
+//  Created by Fabian Delano Kurnia on 15/06/21.
+//
+
+import UIKit
+
+class FreePlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
--- BimbarAngklungApp/ViewController.swift
+++ BimbarAngklungApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mainImage.image = UIImage(named: "AngklungTest")
+        mainImage.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
     }
 
