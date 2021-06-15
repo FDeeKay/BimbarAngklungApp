ebe861d6ce1b95d00023fb6a7a56919f6e11d12e
Fabian Delano Kurnia <fabiandkurnia@gmail.com>
2021-06-15 14:32:18 +0700

    Add LevelSelectionViewController.swift, reconnect segue to LevelSelection View Controller

---
+++ BimbarAngklungApp/LevelSelectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LevelSelectionViewController.swift
+//  BimbarAngklungApp
+//
+//  Created by Fabian Delano Kurnia on 15/06/21.
+//
+
+import UIKit
+
+class LevelSelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
