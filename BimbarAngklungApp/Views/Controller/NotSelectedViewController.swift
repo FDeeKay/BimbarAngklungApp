355d6ffe92b2352cc0328439a8c2641138cee909
Tsamara Alifia <tsamara27@gmail.com>
2021-06-24 03:15:41 +0700

    Perubahan mainly ngerapihin UI di bagian background dan layouting

--- BimbarAngklungApp/Views/Controller/NotSelectedViewController.swift
+++ BimbarAngklungApp/Views/Controller/NotSelectedViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class NotSelectedViewController: UIViewController {
-
+    @IBOutlet weak var NextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NextButton.layer.cornerRadius = 20
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
